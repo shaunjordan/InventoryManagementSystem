44d83b51a29c9d4382ea25a5e637e3504302f425
Shaun Jordan <s1bravo@gmail.com>
2018-09-19 22:27:14 -0500

    init commit

---
+++ InventoryManagementLibrary/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementLibrary
+{
+    public class Inventory
+    {
+        List<Product> Products = new List<Product>;
+        List<Part> AllParts = new List<Part>;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name=""></param>
+        public void AddProduct(product) { }
+
+        public bool RemoveProduct(int) { }
+
+        public Product LookupProduct(int) { }
+
+        public void UpdateProudct(int, product) { }
+
+        public void AddPart(part) { }
+
+    }
+}
